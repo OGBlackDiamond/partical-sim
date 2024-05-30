acbfa34102590ed8ca5eb169240dab09abea118b
Caden Feller <caden.feller@gmail.com>
2024-05-30 11:15:31 -0700

    started raycast structure

--- simulation/src/main/java/dev/Main.java
+++ simulation/src/main/java/dev/Main.java
@@ -14,18 +14,18 @@ class Main {
         
         Window window = new Window(1000, 1000);
 
-        Engine engine = new Engine(window.getWidth(), window.getHeight());
+        Engine engine = new Engine(window.getWidth(), window.getHeight(), 1, 1);
 
 
-        window.updateBuffer(engine.getBuffer());
+        window.updateBuffer(engine.getBuffer().getBuffer());
    
         int ticker = 0;
         while (System.nanoTime() - startTime > 50000000) {
             if (System.nanoTime() % 5000 == 0) {
-                engine.fill(ticker, ticker, ticker, 255);
+                engine.getBuffer().fill(ticker, ticker, ticker, 255);
                 if (ticker < 255)
                     ticker++;
-                window.updateBuffer(engine.getBuffer());
+                window.updateBuffer(engine.getBuffer().getBuffer());
             }
         }
     }
--- simulation/src/main/java/dev/engine/Engine.java
+++ simulation/src/main/java/dev/engine/Engine.java
@@ -7,58 +7,134 @@ public class Engine {
 
     BufferedImage imgBuffer;
 
-
-
     int width;
     int height;
 
-    public Engine(int width, int height) {
-        generateBuffer(width, height, true);
+    double FOV;
+    double checkRate;
+
+    Camera cam;
+    BuffWrapper buffer;
+
+    public Engine(int width, int height, double FOV, double checkRate) {
         this.width = width;
         this.height = height;
-        fill(0, 0, 0, 0);
+        this.FOV = FOV;
+        this.checkRate = checkRate;
+        cam = new Camera(0, 0, 0);
+        buffer = new BuffWrapper(width, height, width / 2, height / 2, FOV);
+
     }
 
-    /**
-     * Generates a fresh buffer at the specified dimensions with optional
-     * transparency support.
+    private void castRays() {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                double[] camPos = cam.getPos();
+                Vector ray = new Vector(camPos[0], camPos[1], camPos[2], x, y);
 
-     * @param width       - the width of the buffer
-     * @param height      - the hight of the buffer
-     * @param transparent - whether to support transparency in the image
-     */
-    public void generateBuffer(int width, int height, boolean transparent) {
-        imgBuffer = new BufferedImage(width, height,
-                transparent ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB);
-    }
-    
 
-    private void setPixel (int x, int y, int r, int g, int b, int a) {
-        imgBuffer.setRGB(x, y, new Color(r, g, b, a).getRGB());
-    }
 
-    public void fill(int r, int g, int b, int a) {
-        for (int x = 0; x < imgBuffer.getWidth(); x++) {
-            for (int y = 0; y < imgBuffer.getHeight(); y++) {
-                setPixel(x, y, r, g, b, a);
             }
         }
     }
 
-    public BufferedImage getBuffer() {
-        return imgBuffer;
+    public BuffWrapper getBuffer() {
+        return buffer;
     }
-}
 
-class Camera {
+    class Camera {
 
-    private int X;
-    private int Y;
-    private int Z;
-    
-    public Camera(int X, int Y, int Z) {
-        this.X = X;
-        this.Y = Y;
-        this.Z = Z;
+        private double x;
+        private double y;
+        private double z;
+
+        public Camera(double x, double y, double z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public double[] getPos() {
+            return new double[] {x, y, z};
+        }
+    }
+
+    public class BuffWrapper {
+
+        private BufferedImage buffer;
+
+        private double x;
+        private double y;
+        private double z;
+
+        public BuffWrapper(int width, int height, double camX, double camY, double camZ) {
+            generateBuffer(width, height, true);
+            fill(0, 0, 0, 0);
+        }
+
+        /**
+         * Generates a fresh buffer at the specified dimensions with optional
+         * transparency support.
+         * 
+         * @param width       - the width of the buffer
+         * @param height      - the hight of the buffer
+         * @param transparent - whether to support transparency in the image
+         */
+        public void generateBuffer(int width, int height, boolean transparent) {
+            imgBuffer = new BufferedImage(width, height,
+                    transparent ? BufferedImage.TYPE_INT_ARGB : BufferedImage.TYPE_INT_RGB);
+        }
+
+        private void setPixel(int x, int y, int r, int g, int b, int a) {
+            imgBuffer.setRGB(x, y, new Color(r, g, b, a).getRGB());
+        }
+
+        public void fill(int r, int g, int b, int a) {
+            for (int x = 0; x < imgBuffer.getWidth(); x++) {
+                for (int y = 0; y < imgBuffer.getHeight(); y++) {
+                    setPixel(x, y, r, g, b, a);
+                }
+            }
+        }
+
+        public BufferedImage getBuffer() {
+            return imgBuffer;
+        }
+
+    }
+
+    class Vector {
+
+        private double startX;
+        private double startY;
+        private double startZ;
+
+        private double incrimentX;
+        private double incrimentY;
+        private double incrimentZ;
+
+        public Vector(double startX, double startY, double startZ, int buffX, int buffY) { 
+            this.startX = startX;
+            this.startY = startY;
+            this.startZ = startZ;
+
+            this.incrimentX = startX - (width / 2) + buffX;
+            this.incrimentY = startY - (height / 2) + buffY;
+            this.incrimentZ = FOV / checkRate;
+        }
+
+        public Vector() {
+            this(0, 0, 0, 0, 0);
+        }
+
+        public double[] getPos() {
+            return new double[] { startX, startY, startZ };
+        }
+
+        public void incriment() {
+            startX += incrimentX;
+            startY += incrimentY;
+            startZ += incrimentZ;
+        }
     }
 }
